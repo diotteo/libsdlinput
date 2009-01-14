c418fd4011faa18c9fce27af006d6bed274a6d1e
Olivier Diotte <olivier@diotte.ca>
2009-01-14 03:47:31 +0000

    Commit archive SDL_Input_TTF/SDL_Input_TTF-1.0.1/SDL_Input_TTF-1.0.1-src.tar.gz
    Timestamp: 2009-01-14 03:47:31 UTC

--- SDL_Input_TTF.c
+++ SDL_Input_TTF.c
@@ -3,16 +3,16 @@
 This file is part of the SDL_Input_TTF library
 
 SDL_Input is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public
+modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
 version 2.1 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-General Public License for more details.
+Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public
+You should have received a copy of the GNU Lesser General Public
 License along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA */
 
@@ -29,7 +29,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA */
  *Purpose: Text-blitting functions
  *Author: Olivier Diotte
  *Created on: March 17th 2008
- *Last update: Oct. 4th, 2008
+ *Last update: Oct. 9th, 2008
  */
 
 #include <stdio.h>
@@ -38,7 +38,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA */
 #include <SDL/SDL.h>
 #include <SDL/SDL_ttf.h>
 
-#include "../SDL_Input.h"
+#include "SDL_Input.h"
 #include "SDL_Input_TTF.h"
 
 /*Checks if text can be displayed. Returns 0 if successful or -1 otherwise.*/
--- SDL_Input_TTF.h
+++ SDL_Input_TTF.h
@@ -3,16 +3,16 @@
 This file is part of the SDL_Input_TTF library
 
 SDL_Input is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public
+modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
 version 2.1 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-General Public License for more details.
+Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public
+You should have received a copy of the GNU Lesser General Public
 License along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA */
 
@@ -36,7 +36,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA */
 
 #define SDL_INPUT_TTF_MAJOR_VERSION 1
 #define SDL_INPUT_TTF_MINOR_VERSION 0
-#define SDL_INPUT_TTF_PATCHLEVEL	0
+#define SDL_INPUT_TTF_PATCHLEVEL	1
 
 #define SDL_INPUT_TTF_VERSION( X )				\
 {												\
